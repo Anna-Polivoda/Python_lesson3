cad0cd3bc8e77b013289d8a600346c939e971433
Anna-Polivoda <anny.b@mail.ru>
2023-04-23 21:41:37 +0300

    Added task19.py

--- task19.py
+++ task19.py
@@ -10,27 +10,3 @@ arr2=arr[-k:] + arr[:-k]
 print(arr2)
 
 
-
-
-
-# вторая задача: list_1 = [0, -1, 5, 2, 3]
-# count = 0
-# for i in range(1, len(list_1)):
-#     if list_1[i] > list_1[i-1]:
-#         count += 1
-# print(count)
-
-
-
-# #Задача 23
-
-# arr = [0, -1, 5, 2, 3, 4, 0]
-# count = 0
-
-# for x in range(1, len(arr)):
-#     if arr[x - 1] < arr[x]:
-#         count += 1
-
-# print(count)
-
- 
